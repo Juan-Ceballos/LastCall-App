b0dbf0a119e2434527abb2314f65387140bf95be
Juan C <juanceballos@pursuit.org>
2021-09-08 22:58:47 -0400

    fc

---
+++ LastCall-App/LastCall-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LastCall-App
+//
+//  Created by Juan Ceballos on 9/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
