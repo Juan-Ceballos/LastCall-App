14b2d5802e0e636c5d5cc13a620cec3b72d427a9
Jaheed <jaheedhaynes@pursuit.org>
2021-09-09 00:11:35 -0400

    Added Tab Bar Controller, Added Firebase Logic

---
+++ LastCall-App/LastCall-App/Controllers/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  LastCall-App
+//
+//  Created by Jaheed Haynes on 9/9/21.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewControllers = [ViewController()]
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
